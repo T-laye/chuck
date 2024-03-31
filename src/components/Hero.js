77c7f41bf0fc299473627aabf6c28f6ad9ef7f98
Laye <tlayefaah@gmail.com>
2024-03-31 21:47:56 +0100

    init commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="min-h-screen pt-16 pb-20 border-b border-b-[#FFFFFF20] ">
+    <div className="min-h-screen pt-16 lg:pt-24 pb-20 border-b border-b-[#FFFFFF20] ">
       <DropBoxes />
       <div className="container mx-auto px-4 flex flex-col lg:flex-row lg:h-screen gap-10">
         <div className="flex flex-col mt-0 gap-5 items-center lg:items-start pt-10 lg:justify-start lg:mt-10 lg:w-1/2 lg:h-full bg-rd-400">
